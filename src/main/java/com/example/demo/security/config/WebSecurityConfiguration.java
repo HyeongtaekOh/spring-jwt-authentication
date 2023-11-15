e0c901fcba3d8dacef4e2b2c88a7478fdc3fe600
HyeongtaekOh <oh052679@gmail.com>
2023-11-16 00:05:15 +0900

    add dependencies for oauth2

---
+++ src/main/java/com/example/demo/security/config/WebSecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.example.demo.security.config;
+
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+public class WebSecurityConfiguration extends ResourceServerConfigurerAdapter {
+}
